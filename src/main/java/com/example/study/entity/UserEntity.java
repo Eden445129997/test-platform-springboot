c9422d12aa1166ee1d4324e1eb2c5e26d099c56e
445129997@qq.com <445129997@qq.com>
2019-11-21 17:06:20 +0800

    first commit

---
+++ src/main/java/com/example/study/entity/UserEntity.java
@@ -0,0 +1,23 @@
+package com.example.study.entity;
+
+public class UserEntity {
+    private Integer id;
+    private String uname;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getUname() {
+        return uname;
+    }
+
+    public void setUname(String uname) {
+        this.uname = uname;
+    }
+
+}
