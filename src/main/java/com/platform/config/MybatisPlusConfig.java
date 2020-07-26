594f35f466082eacce590f48301befe10bf90b5c
洪文辉 <Eden445129997@163.com>
2020-07-26 22:35:03 +0800

    去除无用依赖

--- src/main/java/com/platform/config/MybatisPlusConfig.java
+++ src/main/java/com/platform/config/MybatisPlusConfig.java
@@ -1,7 +1,6 @@
 package com.platform.config;
 
 import com.baomidou.mybatisplus.extension.plugins.PaginationInterceptor;
-import com.baomidou.mybatisplus.extension.plugins.PerformanceInterceptor;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -27,11 +26,4 @@ public class MybatisPlusConfig {
         return new PaginationInterceptor();
     }
 
-    /**
-     * SQL执行效率插件
-     */
-    @Bean
-    public PerformanceInterceptor performanceInterceptor() {
-        return new PerformanceInterceptor();
-    }
 }
